f0264e3a819501271f569e19d7e5fbfbde5dd4f4
Vinit <vinitnagar56@gmail.com>
2024-11-29 01:12:46 +0530

    minor change for testing purpose

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),  // Set alias for '@' to the 'src' folder
+      "@": path.resolve(__dirname, "./src"),  //Set alias for '@' to the 'src' folder
     },
   },
 });
