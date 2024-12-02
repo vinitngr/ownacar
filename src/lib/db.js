2ea25eefb520e166cd606f6e1fd017983b9d30b1
vinit <vinitnagar56@gmail.com>
2024-12-02 23:08:21 +0530

    xs change

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,6 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import * as schema from "../lib/schema.js";
 
 const neon_env_key= import.meta.env.VITE_NEON_DATABASE_URL
 const sql = neon(neon_env_key);
-export const db= drizzle(sql , {schema});
+export const db= drizzle(sql);
